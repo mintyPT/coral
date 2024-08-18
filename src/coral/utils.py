7fc0528c50c60958bc657ac3e7bf19fce3050bed
mauro <mauro@scandit.com>
2024-08-18 23:14:59 +0100

    Add types and docstrings

--- src/coral/utils.py
+++ src/coral/utils.py
@@ -1,5 +1,5 @@
 import functools
-from typing import Any
+from typing import Any, Callable
 
 
 def flatten(arr: list[list[Any]]) -> list[Any]:
@@ -16,5 +16,27 @@ def flatten(arr: list[list[Any]]) -> list[Any]:
     return [element for subarr in arr for element in subarr]
 
 
-def apply_functions(functions, initial_value):
+def apply_functions(functions: list[Callable[[Any], Any]], initial_value: Any) -> Any:
+    """
+    Applies a list of functions sequentially to an initial value.
+
+    This function takes a list of functions and an initial value. It applies
+    each function in the list to the result of the previous function, starting
+    with the initial value, and returns the final result.
+
+    :param functions: A list of functions to be applied sequentially. Each function
+                      must take one argument and return a value of the same type.
+    :param initial_value: The initial value to be transformed by the functions.
+    :return: The final result after all functions have been applied.
+
+    Example:
+    >>> def add_one(x: int) -> int:
+    >>>     return x + 1
+    >>>
+    >>> def multiply_by_two(x: int) -> int:
+    >>>     return x * 2
+    >>>
+    >>> apply_functions([add_one, multiply_by_two], 5)
+    12
+    """
     return functools.reduce(lambda value, func: func(value), functions, initial_value)
