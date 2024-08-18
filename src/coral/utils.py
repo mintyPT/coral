cb1caf76023142f52c1fc1ac6d9313acebf683fb
mauro <mauro@scandit.com>
2024-08-18 23:29:22 +0100

    Add types and dostrings

--- src/coral/utils.py
+++ src/coral/utils.py
@@ -1,6 +1,8 @@
 import functools
+from typing import TypeVar
 from pathlib import Path
 from typing import Any, Callable
+from typing import Generator
 
 
 def flatten(arr: list[list[Any]]) -> list[Any]:
@@ -43,7 +45,21 @@ def apply_functions(functions: list[Callable[[Any], Any]], initial_value: Any) -
     return functools.reduce(lambda value, func: func(value), functions, initial_value)
 
 
-def remove_dups(arr):
+T = TypeVar("T")
+U = TypeVar("U")
+
+
+def remove_dups(arr: list[T]) -> list[T]:
+    """
+    Removes duplicates from the input list while preserving the original order.
+
+    :param arr: A list that may contain duplicate elements.
+    :return: A list with duplicates removed, maintaining the original order of elements.
+
+    Example:
+    >>> remove_dups([1, 2, 3, 2, 1])
+    [1, 2, 3]
+    """
     ret = []
     for a in arr:
         if a not in ret:
@@ -51,11 +67,32 @@ def remove_dups(arr):
     return ret
 
 
-def iter_tree(path: Path):
+def iter_tree(path: Path) -> Generator[Path, None, None]:
+    """
+    Iteratively yields a path and all its parent directories up to the root.
+
+    :param path: A file or directory path.
+    :yield: The input path and each of its parent directories, starting from the input path itself.
+
+    Example:
+    >>> list(iter_tree(Path("/a/b/c")))
+    [PosixPath('/a/b/c'), PosixPath('/a/b'), PosixPath('/a'), PosixPath('/')]
+    """
     yield path
     if path.parent and path.parent != path:
         yield from iter_tree(path.parent)
 
 
-def map_func(func):
+def map_func(func: Callable[[T], U]) -> Callable[[list[T]], list[U]]:
+    """
+    Returns a function that applies the provided function to each element of a list.
+
+    :param func: A function that takes an element of type T and returns a result of type U.
+    :return: A function that takes a list of elements of type T and returns a list of elements of type U.
+
+    Example:
+    >>> multiply_by_two = map_func(lambda x: x * 2)
+    >>> multiply_by_two([1, 2, 3])
+    [2, 4, 6]
+    """
     return lambda arr: list(map(func, arr))
